290fc3c91f8f51e81ee84c53437f356398b812d7
DmitriyKim01 <gitlab.vitality110@passmail.net>
2024-10-02 15:12:22 -0400

    Fix the return of the add and get event

--- milestone2/event_queue.py
+++ milestone2/event_queue.py
@@ -12,34 +12,26 @@ class EventQueue:
             3: "Raccoon Scavenging",
             4: "Bird Gathering"
         }
-        self.lock = threading.Lock() 
+        self.execute_lock = threading.Lock() 
         self.get_lock = threading.Lock()
         self.get_lock.acquire()
 
     def add_event(self, id):
         if id not in self.event_id_name_map:
             print(f"Invalid event ID: {id}")
-            return None
+            raise Exception(f"Invalid event ID: {id}")
 
-        with self.lock: 
+        with self.execute_lock: 
             current_time = time.time()
             self.events.append(self.event_id_name_map[id]) 
             self.timestamps.append(current_time) 
             self.get_lock.release()
             print(f'Event added: {id} - {self.event_id_name_map[id]}')
             print(f'Timestamp added: {current_time}')
-            return True
-
-        return None
 
     def get_event(self):
           self.get_lock.acquire()
-          with self.lock:
-              if (len(self.events) < 0 or len(self.timestamps) < 0):
-                  print(f'The even queue or timestamp list is empty!!!')
-                  return None
-              else:
-                  return (self.events[-1], self.timestamps[-1])
-          return None
+          with self.execute_lock:
+            return self.events.pop()
 
 
