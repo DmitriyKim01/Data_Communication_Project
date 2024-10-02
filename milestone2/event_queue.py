cd314771d05d0f8e02c14c3cc7e30aa937e6e7e9
DmitriyKim01 <gitlab.vitality110@passmail.net>
2024-10-02 14:51:45 -0400

    Implement add and get event methods

--- milestone2/event_queue.py
+++ milestone2/event_queue.py
@@ -1,20 +1,45 @@
-class EventQueue():
+import time
+import threading
+
+class EventQueue:
     def __init__(self):
-        self.events = []
-        self.timestamps = []
-        self.event_id_name_map = {0:'Example'} #Replace with your own map
+        self.events = []  
+        self.timestamps = [] 
+        self.event_id_name_map = {
+            0: "Deer Grazing",
+            1: "Squirrel Running",
+            2: "Bear Sleeping",
+            3: "Raccoon Scavenging",
+            4: "Bird Gathering"
+        }
+        self.lock = threading.Lock() 
+        self.get_lock = threading.Lock()
+        self.get_lock.acquire()
 
-    '''
-    Adds events to the event array  and timestamps to the timestamp array
-    '''
-    def add_event(self,id):
-        print(f'add_event: {id} {self.event_id_name_map[id]}')
-        pass
+    def add_event(self, id):
+        if id not in self.event_id_name_map:
+            print(f"Invalid event ID: {id}")
+            return None
 
-    '''
-    Pops events from the event array and timestamp array
-    If no event available, blocks until one becomes available.
-    (i.e. Should always return an event.)
-    '''
-    def get_event(self):
+        with self.lock: 
+            current_time = time.time()
+            self.events.append(self.event_id_name_map[id]) 
+            self.timestamps.append(current_time) 
+            self.get_lock.release()
+            print(f'Event added: {id} - {self.event_id_name_map[id]}')
+            print(f'Timestamp added: {current_time}')
+            return True
+
         return None
+
+    def get_event(self):
+          self.get_lock.acquire()
+          with self.lock:
+              if (len(self.events) < 0 or len(self.timestamps) < 0):
+                  print(f'The even queue or timestamp list is empty!!!')
+                  return None
+              else:
+                  return (self.events[-1], self.timestamps[-1])
+          return None
+
+
