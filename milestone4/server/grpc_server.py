88d5789fd08af0bffd2ee012e7fb66d5d14d0bc2
DmitriyKim01 <dmitriykim.office@protonmail.com>
2024-11-23 17:22:42 +0000

    Add public key exchange to server

--- milestone4/server/grpc_server.py
+++ milestone4/server/grpc_server.py
@@ -6,6 +6,7 @@ import grpc as grpc
 from concurrent import futures
 from proto import sensor_pb2
 from proto import sensor_pb2_grpc as grpc_sensor
+from cryptography.hazmat.primitives import serialization
     
 class SensorServiceServicer(grpc_sensor.SensorServerServicer):
     def __init__(self):
@@ -14,11 +15,12 @@ class SensorServiceServicer(grpc_sensor.SensorServerServicer):
     # Capture an image from a sensor
     def TriggerCapturePc(self, request, context):
         try:
-            current_sensor_ip = self.sensors[request.id]
+            print(f'Capture was triggered by PC for sensor {request.sensor_id}')
+            current_sensor_ip = self.sensors[request.sensor_id]
             channel = grpc.insecure_channel(current_sensor_ip)  
             stub = grpc_sensor.SingleSensorStub(channel)
-            trigger_request = sensor_pb2.TriggerRequest(id=request.id)
-            response = stub.TriggerCapture(trigger_request)
+            response = stub.TriggerCapture(request)
+            print('Capture response:', response)
             return response
         except grpc.RpcError as e:
             context.set_code(grpc.StatusCode.INTERNAL)
@@ -36,14 +38,28 @@ class SensorServiceServicer(grpc_sensor.SensorServerServicer):
         sensor_ids = list(self.sensors.keys())  
         return sensor_pb2.AvailableSensors(ids=sensor_ids)
     
+    def SendPublicKeyToServer(self, request, context):
+        try:
+            print(f"Received public key from sensor {request.sensor_id}: {request.public_key}")
+            current_sensor_ip = self.sensors[request.sensor_id]
+            channel = grpc.insecure_channel(current_sensor_ip)
+            stub = grpc_sensor.SingleSensorStub(channel)
+            response = stub.SendPublicKeyToSensor(request)
+            print('Public key sent to sensor:', response.message)
+            return response
+        except grpc.RpcError as e:
+            context.set_code(grpc.StatusCode.INTERNAL)
+            context.set_details(f"Error during gRPC call: {e}")
+            return sensor_pb2.PublicKeyResponse
+    
 def serve():
     # Create the server and add the servicer
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     grpc_sensor.add_SensorServerServicer_to_server(SensorServiceServicer(), server)
 
     # Bind the server to a port
-    server.add_insecure_port('[::]:50051')  
-    print("Server started, listening on port 50051...")
+    server.add_insecure_port('[::]:7070')  
+    print("Server started, listening on port 7070...")
 
     # Start the server
     server.start()
